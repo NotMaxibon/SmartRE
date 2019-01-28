65bf8c90305451d9d8e1e88e3781468ce26c098c
NotMaxibon <46921340+NotMaxibon@users.noreply.github.com>
2019-01-29 00:34:42 +0100

    AppDesktop

---
+++ ClassTrainer/src/classtrainer/Student.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classtrainer;
+import java.lang.String;
+/**
+ *
+ * @author Maximilian
+ */
+public class Student{
+    public String n;
+    public String s;
+    public String dir;
+    public Student(String Name,String Surname,String Directory){
+        n=Name;
+        s=Surname;
+        dir=Directory;
+    }
+}
